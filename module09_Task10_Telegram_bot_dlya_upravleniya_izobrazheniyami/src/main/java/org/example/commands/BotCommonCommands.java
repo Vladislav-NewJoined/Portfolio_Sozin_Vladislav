914b25390d1d63c70ddc2abf9dc9d4c01620314d
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-12-29 16:10:47 +0300

    241229 commit

---
+++ module09_Task10_Telegram_bot_dlya_upravleniya_izobrazheniyami/src/main/java/org/example/commands/BotCommonCommands.java
@@ -0,0 +1,18 @@
+package org.example.commands;
+
+public class BotCommonCommands {
+    @AppBotCommand(name = "/hello", description = "when request hello", showInHelp = true)
+    public String hello() {
+        return "Hello, User";
+    }
+
+    @AppBotCommand(name = "/bye", description = "when request bye", showInHelp = true)
+    public String bye() {
+        return "Good bye, User";
+    }
+
+    @AppBotCommand(name = "/help", description = "when request help", showInKeyboard = true)
+    public String help() {
+        return "HEEEELP";
+    }
+}
---
+++ module09_Task10_Telegram_bot_dlya_upravleniya_izobrazheniyami/src/main/java/org/example/functions/ImageOperation.java
@@ -0,0 +1,7 @@
+package org.example.functions;
+
+import java.lang.reflect.InvocationTargetException;
+
+public interface ImageOperation {
+    float[] execute(float[] rgb) throws InvocationTargetException, IllegalAccessException;
+}
