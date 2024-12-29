143e36d65649582c39103da5ca8c0c407026ff63
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-12-29 18:40:54 +0300

    241229 commit

---
+++ module15_Task1_Poisk_rabotyi_i_prodvinyutyi_temy/src/main/java/org/example/zadanye1/Main1.java
@@ -0,0 +1,34 @@
+package org.example.zadanye1;
+
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("""
+            Модуль 15. Поиск работы и продвинутые темы. Задание №1. Проект:\s
+                Шаг 1:
+                Создаём учётную запись
+                Перейдите на сайт github.com, зарегистрируйтесь и верифицируйте адрес электронной
+                почты. Выберите тип аккаунта: публичный или приватный. В публичном аккаунте
+                репозитории видны всем, а в приватном — только тем участникам, которым вы
+                сами открыли доступ.
+
+            Решение:
+            """);
+
+        System.out.println("""
+                На веб-сайте: https://github.com/
+                Создан мой аккаунт (или profile), и в нём созданы мои репозитории.
+                Имя моего аккаунта (или логин):  Vladislav-NewJoined
+                Почта, указанная при регистрации: sozin.vladislav@mail.ru
+                Ссылка на мой аккаунт:\s
+                https://github.com/Vladislav-NewJoined
+                Пример моего репозитория для этого задания:\s
+                Имя репозитория:
+                n240221_Module1_Task1_Zadaniya1-16
+                Тип репозитория: Public
+                Ссылка на репозиторий:\s
+                https://github.com/Vladislav-NewJoined/n240221_Module1_Task1_Zadaniya1-16.git
+            """);
+
+
+    }
+}
